d8a4c6e42f1dc9bcbd1bfd81ccf0f3e374abdd81
Rafi Satria <50817438+ThatGuyNameZack@users.noreply.github.com>
2025-09-10 14:20:35 +0700

    Change file extension from .jpg to .png

--- page_order.py
+++ page_order.py
@@ -5,7 +5,7 @@ def rename_images_in_folder(folder_path):
     for i, filename in enumerate(files):
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path):
-            new_name = f"{i+1:03}.jpg"
+            new_name = f"{i+1:03}.png"
             new_path = os.path.join(folder_path, new_name)
             os.rename(file_path, new_path)
     print(f"Renamed {len(files)} files in {folder_path} to be in order in numbers.")
